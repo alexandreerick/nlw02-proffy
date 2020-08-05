baad6515d447c83915511df033a075b25164cb4f
alexandreerick <erickalexandrex@gmail.com>
2020-08-04 21:49:58 -0300

    backend finalizado

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 
 const routes = express.Router();
-const classesControlers = new ClassesController()
+const classesController = new ClassesController()
+const connectionsController = new ConnectionsController()
 
-routes.post('/classes', classesControlers.create);
-routes.get('/classes', classesControlers.index);
+routes.post('/classes', classesController.create);
+routes.get('/classes', classesController.index);
+
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
+
 
 export default routes;
